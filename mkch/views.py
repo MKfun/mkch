c61c11e0ed12562f111989a46374865d25db84e1
Lain Iwakura <lain@iwakurahome.ru>
2025-10-07 20:48:07 +0300

    ура версия в настройках

--- mkch/views.py
+++ mkch/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import get_object_or_404, render
+import os
+import subprocess
+
+
+def _get_commit_short():
+    try:
+        return subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=os.path.dirname(os.path.dirname(__file__))).decode().strip()
+    except Exception:
+        return "unknown"
+
+
+COMMIT_SHORT = _get_commit_short()
 
 def settings_view(request):
-    return render(request, 'settings.html', {'nsfw': True if request.COOKIES.get('blur-nsfw') == '1' else False, 'animations': True if request.COOKIES.get('animations') == '1' else False})
+    return render(request, 'settings.html', {'nsfw': True if request.COOKIES.get('blur-nsfw') == '1' else False, 'animations': True if request.COOKIES.get('animations') == '1' else False, 'version': COMMIT_SHORT})
