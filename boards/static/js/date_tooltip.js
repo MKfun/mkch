1452fc5f20d381375d28c9722d350b1598e7b344
TimurSennikov <timursennikov1@gmail.com>
2025-10-09 02:02:08 +0300

    new date popups

---
+++ boards/static/js/date_tooltip.js
@@ -0,0 +1,22 @@
+function createDateToolTipListeners() {
+	let elems = document.querySelectorAll(".post-date");
+
+	for(let elem of elems) {
+		let date_detail = elem.querySelector(".date-detail");
+
+		elem.addEventListener("mouseenter", (e) => {
+			date_detail.hidden = false;
+
+			date_detail.style.left = e.pageX + "px";
+			date_detail.style.top = e.pageY + "px";
+		});
+
+		elem.addEventListener("mouseleave", () => {
+			date_detail.hidden = true;
+		});
+	}
+}
+
+window.addEventListener("load", () => {
+	createDateToolTipListeners();
+});
